104dbacf64f2a43b9c925f6733edb6921efbb07f
Mathieu Pelletier <mkpelletier@gmail.com>
2026-05-28 13:30:34 +0200

    Bump version.php to 2026052801 for v2.4.7 release

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_unifiedgrader';
-$plugin->version   = 2026052603; // YYYYMMDDXX format.
+$plugin->version   = 2026052801; // YYYYMMDDXX format.
 $plugin->release   = '2.4.7';
 $plugin->requires  = 2024100700; // Moodle 4.5+.
 $plugin->maturity  = MATURITY_STABLE;
